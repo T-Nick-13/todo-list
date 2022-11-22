60bffe6a84765a34b2c8e680e038f97c7eb8b18c
Nikita Titov <netitov@gmail.com>
2022-11-23 00:12:02 +0300

    front: making header

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+/* import Api from '../../utils/Api';
+import { MAIN_API } from '../../utils/config'; */
+
+import Header from '../Header/Header';
+
+
+function App() {
+
+
+  return (
+    <div className="page">
+      <div className="page__container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+
+
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h1 className="header__title">Todo-list</h1>
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
